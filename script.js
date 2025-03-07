e0ecfafa56cdc12f8aa7ffc7fa187d628980599b
anuj1012 <anujshakya.3m@gmail.com>
2025-03-07 23:04:12 +0530

    order page

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js
+
+// Function to redirect to the buy page with product details
+function redirectToBuyPage(imageSrc, productName, price) {
+    // Store product details in localStorage
+    localStorage.setItem('productImage', imageSrc);
+    localStorage.setItem('productName', productName);
+    localStorage.setItem('productPrice', price);
+    
+    // Redirect to the buy page
+    window.location.href = 'buy.html';
+}
+
+function redirectToBuy(image,name,price){
+
+    localStorage.setItem('productImage',image),
+    localStorage.setItem('productName',name),
+    localStorage.setItem('productPrice',price);
+
+    window.location.href='buy.html';
+}
